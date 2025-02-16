6eed4128467bd16babba88a8465e7bef5d185840
Vishwa GW <136871744+vishwagw@users.noreply.github.com>
2025-02-16 09:27:19 +0530

    Create preproccesing.py

---
+++ preproccesing.py
@@ -0,0 +1,3 @@
+# this file contains the python file for data preprocessing.
+# Data should be organanized, reshaped, cropped and clean the dataset.
+# for full process, use the 'training.py' script.
